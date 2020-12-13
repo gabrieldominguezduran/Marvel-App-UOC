93ca5faa4845ea61c595c22ea1fed967b4bb12df
Gabriel Dominguez Duran <gabrieldominguezduran@gmail.com>
2020-12-13 19:03:10 +0100

    Fix padding on logo

--- docs/trina_gallery.js
+++ docs/trina_gallery.js
@@ -11,6 +11,7 @@ fetch(URL2)
       let img = document.createElement('img');
       img.src = `${c.thumbnail.path}.${c.thumbnail.extension}`;
       img.alt = `Portada del comic ${c.title}`;
+      img.longdesc = `${c.title}`;
       img.loading = `lazy`;
       trinaComics.appendChild(div);
       div.appendChild(img);
